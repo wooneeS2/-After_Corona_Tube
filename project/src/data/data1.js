12e0b90a8a0b454157cc649e16aacb37b7392b56
woonees2 <hello_world_@kakao.com>
2021-12-30 23:04:25 +0900

    Remove json file

---
+++ project/src/data/data1.js
@@ -0,0 +1,252 @@
+// data 1-2
+export const monthlyAudience = [
+  { year: 2020, month: 8, audience: 14737078 },
+  { year: 2020, month: 9, audience: 2704952 },
+  { year: 2020, month: 10, audience: 7099346 },
+  { year: 2020, month: 11, audience: 5677182 },
+  { year: 2020, month: 12, audience: 1201264 },
+  { year: 2021, month: 1, audience: 270830 },
+  { year: 2021, month: 2, audience: 1353374 },
+  { year: 2021, month: 3, audience: 760434 },
+  { year: 2021, month: 4, audience: 2198684 },
+  { year: 2021, month: 5, audience: 1575612 },
+  { year: 2021, month: 6, audience: 1375828 },
+  { year: 2021, month: 7, audience: 4219478 },
+  { year: 2021, month: 8, audience: 12030180 },
+  { year: 2021, month: 9, audience: 5303000 },
+  { year: 2021, month: 10, audience: 1617230 },
+  { year: 2021, month: 11, audience: 3319864 },
+];
+//data 1-3
+export const monthlyRating = [
+  { year: 2020, month: 8, rating: 17620000 },
+  { year: 2020, month: 9, rating: 17930000 },
+  { year: 2020, month: 10, rating: 17930000 },
+  { year: 2020, month: 11, rating: 17630000 },
+  { year: 2020, month: 12, rating: 10930000 },
+  { year: 2021, month: 1, rating: 11020000 },
+  { year: 2021, month: 2, rating: 10820000 },
+  { year: 2021, month: 3, rating: 11070000 },
+  { year: 2021, month: 4, rating: 8009200 },
+  { year: 2021, month: 5, rating: 8002500 },
+  { year: 2021, month: 6, rating: 8000300 },
+  { year: 2021, month: 7, rating: 8006200 },
+  { year: 2021, month: 8, rating: 8005900 },
+  { year: 2021, month: 9, rating: 9003900 },
+  { year: 2021, month: 10, rating: 8000100 },
+  { year: 2021, month: 11, rating: 7009400 },
+];
+
+//data 1-4
+//환자 수 * 100
+export const monthlyPatient = [
+  {
+    Year: 2020,
+    Monthly: 8,
+    patient: 564200,
+  },
+  {
+    Year: 2020,
+    Monthly: 9,
+    patient: 386500,
+  },
+  {
+    Year: 2020,
+    Monthly: 10,
+    patient: 269900,
+  },
+  {
+    Year: 2020,
+    Monthly: 11,
+    patient: 769000,
+  },
+  {
+    Year: 2020,
+    Monthly: 12,
+    patient: 2653900,
+  },
+  {
+    Year: 2021,
+    Monthly: 1,
+    patient: 1746500,
+  },
+  {
+    Year: 2021,
+    Monthly: 2,
+    patient: 1147100,
+  },
+  {
+    Year: 2021,
+    Monthly: 3,
+    patient: 1341200,
+  },
+  {
+    Year: 2021,
+    Monthly: 4,
+    patient: 1891900,
+  },
+  {
+    Year: 2021,
+    Monthly: 5,
+    patient: 1833300,
+  },
+  {
+    Year: 2021,
+    Monthly: 6,
+    patient: 1662100,
+  },
+  {
+    Year: 2021,
+    Monthly: 7,
+    patient: 4138400,
+  },
+  {
+    Year: 2021,
+    Monthly: 8,
+    patient: 5307600,
+  },
+  {
+    Year: 2021,
+    Monthly: 9,
+    patient: 5986800,
+  },
+  {
+    Year: 2021,
+    Monthly: 10,
+    patient: 5341100,
+  },
+  {
+    Year: 2021,
+    Monthly: 11,
+    patient: 8253000,
+  },
+  {
+    Year: 2021,
+    Monthly: 12,
+    patient: 12318400,
+  },
+];
+
+//data1-5
+// avg * 10
+export const monthlyYoutube = [
+  {
+    year: 2020,
+    month: 8,
+    total_view_count: 1468950237,
+    total_numb_video: 808,
+    Avg_view_count: 18180070.71905941,
+  },
+  {
+    year: 2020,
+    month: 9,
+    total_view_count: 1293538539,
+    total_numb_video: 1058,
+    Avg_view_count: 12226260.21833648,
+  },
+  {
+    year: 2020,
+    month: 10,
+    total_view_count: 1845133762,
+    total_numb_video: 1031,
+    Avg_view_count: 17896540.47332687,
+  },
+  {
+    year: 2020,
+    month: 11,
+    total_view_count: 1504312625,
+    total_numb_video: 977,
+    Avg_view_count: 15397260.33060389,
+  },
+  {
+    year: 2020,
+    month: 12,
+    total_view_count: 1022408981,
+    total_numb_video: 847,
+    Avg_view_count: 12070940.42857143,
+  },
+  {
+    year: 2021,
+    month: 1,
+    total_view_count: 1127792854,
+    total_numb_video: 1002,
+    Avg_view_count: 11255410.77045908,
+  },
+  {
+    year: 2021,
+    month: 2,
+    total_view_count: 1074060942,
+    total_numb_video: 777,
+    Avg_view_count: 13823170.81467181,
+  },
+  {
+    year: 2021,
+    month: 3,
+    total_view_count: 1355917667,
+    total_numb_video: 846,
+    Avg_view_count: 16027390.55910166,
+  },
+  {
+    year: 2021,
+    month: 4,
+    total_view_count: 1210460534,
+    total_numb_video: 832,
+    Avg_view_count: 14548800.44951923,
+  },
+  {
+    year: 2021,
+    month: 5,
+    total_view_count: 1955398272,
+    total_numb_video: 792,
+    Avg_view_count: 24689370.21212121,
+  },
+  {
+    year: 2021,
+    month: 6,
+    total_view_count: 1285083366,
+    total_numb_video: 766,
+    Avg_view_count: 16776540.52480418,
+  },
+  {
+    year: 2021,
+    month: 7,
+    total_view_count: 1388105483,
+    total_numb_video: 754,
+    Avg_view_count: 18409880.70424403,
+  },
+  {
+    year: 2021,
+    month: 8,
+    total_view_count: 1378653779,
+    total_numb_video: 758,
+    Avg_view_count: 18188040.45778364,
+  },
+  {
+    year: 2021,
+    month: 9,
+    total_view_count: 1722897251,
+    total_numb_video: 751,
+    Avg_view_count: 22941370.48468708,
+  },
+  {
+    year: 2021,
+    month: 10,
+    total_view_count: 1582942734,
+    total_numb_video: 740,
+    Avg_view_count: 21391110.8027027,
+  },
+  {
+    year: 2021,
+    month: 11,
+    total_view_count: 999303369,
+    total_numb_video: 679,
+    Avg_view_count: 14717280.08394698,
+  },
+  {
+    year: 2021,
+    month: 12,
+    total_view_count: 846630786,
+    total_numb_video: 535,
+    Avg_view_count: 15824870.45046729,
+  },
+];
