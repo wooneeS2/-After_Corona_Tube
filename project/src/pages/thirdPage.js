d0767a00b9711878afec404a24cb23fa88da0f26
woonees2 <hello_world_@kakao.com>
2022-01-01 17:40:24 +0900

    Add page layout

--- project/src/pages/thirdPage.js
+++ project/src/pages/thirdPage.js
@@ -1,7 +1,85 @@
 import React from "react";
+import "../design/thirdPage.css";
 
 export function ThirdPage() {
-  return <div>세번째페이지</div>;
+  return (
+    <div>
+      <div>Title</div>
+
+      <div className="thirdpage-main" style={{}}>
+        <div id="hashtag-box">
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+          <button>qwewewq</button>
+        </div>
+
+        <div id="video1">
+          <p>video title</p>
+          <p>channel</p>
+          <p>channel</p>
+          <p>channel</p>
+        </div>
+
+        <div id="video2">
+          <p>video title</p>
+          <p>channel</p>
+        </div>
+        <div id="video3">
+          <p>video title</p>
+          <p>channel</p>
+        </div>
+        <div id="video4">
+          <p>video title</p>
+          <p>channel</p>
+        </div>
+        <div id="video5">
+          <p>video title</p>
+          <p>channel</p>
+        </div>
+        <div id="video6">
+          <p>video title</p>
+          <p>channel</p>
+        </div>
+        <div id="video7">
+          <p>video title</p>
+          <p>channel</p>
+        </div>
+        <div id="video8">
+          <p>video title</p>
+          <p>channel</p>
+        </div>
+        <div id="video9">
+          <p>video title</p>
+          <p>channel</p>
+        </div>
+
+        <div id="video-next-btn">
+          <button>다음</button>
+        </div>
+        <div id="video-prev-btn">
+          <button>이전</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ThirdPage;
