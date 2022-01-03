ff433840c1f9467fe79b4161a2f0e844c379e946
woonees2 <hello_world_@kakao.com>
2022-01-03 15:16:09 +0900

    fix footer

--- project/src/components/base/footerComponents.js
+++ project/src/components/base/footerComponents.js
@@ -1,6 +1,29 @@
 import "../../design/footer.css";
 
 export function FooterComponents() {
+  const detectUserAgency = () => {
+    var isChromium = window.chrome;
+    var winNav = window.navigator;
+    var vendorName = winNav.vendor;
+    var isOpera = typeof window.opr !== "undefined";
+    var isIEedge = winNav.userAgent.indexOf("Edg") > -1;
+    var isIOSChrome = winNav.userAgent.match("CriOS");
+
+    if (isIOSChrome) {
+    } else if (
+      isChromium !== null &&
+      typeof isChromium !== "undefined" &&
+      vendorName === "Google Inc." &&
+      isOpera === false &&
+      isIEedge === false
+    ) {
+    } else {
+      // FIXME 한번만 눌러도 꺼지게하기
+      window.alert("현재 페이지는 Chrome에 최적화 되어있습니다.");
+    }
+  };
+  detectUserAgency();
+
   return (
     <div className="footer-div">
       <footer>
@@ -8,7 +31,6 @@ export function FooterComponents() {
           <p id="footer-contact">©Elice AI Track 3기 16팀 식스틴</p>
           <p id="footer-contact">contact : @sixteen</p>
         </div>
-        <p id="footer-contact">Chrome에 최적화되어있습니다.</p>
       </footer>
     </div>
   );
