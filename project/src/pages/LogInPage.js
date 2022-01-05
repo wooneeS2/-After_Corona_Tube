840a7691e290668c1ff6503de8d6f201b093048e
woonees2 <hello_world_@kakao.com>
2022-01-05 23:11:08 +0900

    Update sign-in,sign-up validation

--- project/src/pages/LogInPage.js
+++ project/src/pages/LogInPage.js
@@ -1,7 +1,15 @@
 import "../design/logIn.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { isEmail, borderStyle } from "./SignUpPage";
+
+//TODO post 기능
+//TODO 로그인 성공화면 만들기
 
 export function LogInPage() {
+  const [userEmail, setUserEmail] = useState("");
+  const [userPw, setUserPw] = useState("");
+  const [emailError, setEmailError] = useState(false);
   return (
     <>
       <div className="log-in-box">
@@ -9,28 +17,57 @@ export function LogInPage() {
 
         <form>
           <div id="user-id-box">
-            <div id="log-in-id-title">아이디</div>
+            <div id="log-in-id-title">이메일</div>
 
-            <input type="text" name="user-id" id="user-id-input" />
+            <input
+              type="text"
+              name="user-id"
+              id="user-id-input"
+              placeholder="가입한 이메일을 입력해주세요."
+              onChange={e => {
+                setUserEmail(e.target.value);
+              }}
+              style={userEmail !== "" ? borderStyle : {}}
+            />
+            {emailError && (
+              <div style={{ color: "red", fontSize: "1rem" }}>
+                올바른 이메일을 입력해주세요.
+              </div>
+            )}
           </div>
           <div id="user-pw-box">
             <div id="log-in-pw-title">비밀번호</div>
-            <input type="password" name="user-pw" id="user-pw-input" />
+            <input
+              type="password"
+              name="user-pw"
+              id="user-pw-input"
+              placeholder="비밀번호를 입력해주세요."
+              onChange={e => {
+                setUserPw(e.target.value);
+              }}
+              style={userPw !== "" ? borderStyle : {}}
+            />
           </div>
           <button
             type="submit"
             id="user-log-in-btn"
             onClick={e => {
               e.preventDefault();
-              console.log("logIn");
+              const isValidation = isEmail(userEmail);
+              if (!isValidation) {
+                setEmailError(true);
+                console.log("error", emailError);
+              } else {
+                setEmailError(false);
+              }
             }}
           >
             확인
           </button>
         </form>
-        <button id="user-log-in-btn">
-          <Link to="/sign-up">회원가입 하러 가기</Link>
-        </button>
+        <Link to="/sign-up">
+          <button id="user-log-in-btn">회원가입 하러 가기</button>
+        </Link>
       </div>
     </>
   );
--- project/src/pages/SignUpPage.js
+++ project/src/pages/SignUpPage.js
@@ -1,6 +1,19 @@
 import "../design/logIn.css";
+import { useState } from "react";
+
+export const isEmail = email => {
+  const emailRegex =
+    /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+  console.log(email);
+  console.log(emailRegex.test(email));
+  return emailRegex.test(email);
+};
+export const borderStyle = { border: "0.3rem solid #e06666" };
 
 export function SignUpPage() {
+  const [userInfo, setUserInfo] = useState({ name: "", email: "", pw: "" });
+  const [emailError, setEmailError] = useState(false);
+
   return (
     <>
       <div className="log-in-box">
@@ -8,24 +21,73 @@ export function SignUpPage() {
 
         <form>
           <div id="user-id-box">
+            <div id="user-pw-box">
+              <div id="log-in-pw-title">이름</div>
+              <input
+                type="text"
+                name="user-pw"
+                id="user-pw-input"
+                placeholder="이름을 입력해주세요."
+                onChange={e => {
+                  setUserInfo({
+                    ...userInfo,
+                    name: e.target.value,
+                  });
+                }}
+                style={userInfo.name !== "" ? borderStyle : {}}
+              />
+            </div>
             <div id="log-in-id-title">이메일</div>
 
-            <input type="text" name="user-id" id="user-id-input" />
+            <input
+              type="text"
+              name="user-id"
+              id="user-id-input"
+              placeholder="이메일을 입력해주세요."
+              onChange={e => {
+                setUserInfo({
+                  ...userInfo,
+                  email: e.target.value,
+                });
+              }}
+              style={userInfo.email !== "" ? borderStyle : {}}
+            />
+            {emailError && (
+              <div style={{ color: "red", fontSize: "1rem" }}>
+                올바른 이메일을 입력해주세요.
+              </div>
+            )}
           </div>
-          <div id="user-pw-box">
-            <div id="log-in-pw-title">아이디</div>
-            <input type="text" name="user-pw" id="user-pw-input" />
-          </div>
+
           <div id="user-pw-box">
             <div id="log-in-pw-title">비밀번호</div>
-            <input type="password" name="user-pw" id="user-pw-input" />
+            <input
+              type="password"
+              name="user-pw"
+              id="user-pw-input"
+              placeholder="비밀번호를 입력해주세요."
+              onChange={e => {
+                setUserInfo({
+                  ...userInfo,
+                  pw: e.target.value,
+                });
+              }}
+              style={userInfo.pw !== "" ? borderStyle : {}}
+            />
           </div>
           <button
             type="submit"
             id="user-log-in-btn"
             onClick={e => {
               e.preventDefault();
-              console.log("logIn");
+              console.log(userInfo);
+              const isValidation = isEmail(userInfo.email);
+              if (!isValidation) {
+                setEmailError(true);
+                console.log("error", emailError);
+              } else {
+                setEmailError(false);
+              }
             }}
           >
             확인
