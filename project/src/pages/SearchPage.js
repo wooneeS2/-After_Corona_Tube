e1033eec830505de4149328243146a83ff6042f3
woonees2 <hello_world_@kakao.com>
2022-01-07 22:36:15 +0900

    Refactor search page

--- project/src/pages/SearchPage.js
+++ project/src/pages/SearchPage.js
@@ -4,84 +4,14 @@ import "../design/searchPage.css";
 import { GrLike, GrView } from "react-icons/gr";
 import axios from "axios";
 import { GrNext, GrPrevious, GrRefresh } from "react-icons/gr";
-
-const activeStyle = {
-  backgroundColor: "#e0d3d3",
-  fontWeight: "bold",
-  border: "solid 3px #ac8888",
-  boxShadow:
-    "rgba(204, 185, 185, 0.15) 0px 50px 100px -20px, rgba(204, 185, 185, 0.3) 0px 30px 60px -30px, rgba(204, 185, 185, 0.35) 0px -2px 6px 0px inset",
-};
-
-const categoryType = [
-  {
-    category_id: 0,
-    category_name: "전체",
-  },
-  {
-    category_id: 1,
-    category_name: "영화&애니메이션",
-  },
-  {
-    category_id: 2,
-    category_name: "자동차",
-  },
-  {
-    category_id: 10,
-    category_name: "음악",
-  },
-  {
-    category_id: 15,
-    category_name: "동물",
-  },
-  {
-    category_id: 17,
-    category_name: "스포츠",
-  },
-  {
-    category_id: 19,
-    category_name: "여행",
-  },
-  {
-    category_id: 20,
-    category_name: "게임",
-  },
-  {
-    category_id: 22,
-    category_name: "일상",
-  },
-  {
-    category_id: 23,
-    category_name: "코미디",
-  },
-  {
-    category_id: 24,
-    category_name: "엔터테인먼트",
-  },
-  {
-    category_id: 25,
-    category_name: "뉴스",
-  },
-  {
-    category_id: 26,
-    category_name: "노하우",
-  },
-  {
-    category_id: 27,
-    category_name: "교육",
-  },
-  {
-    category_id: 28,
-    category_name: "과학&기술",
-  },
-  {
-    category_id: 29,
-    category_name: "사회&이슈",
-  },
-];
+import { categoryType } from "../data/categoryType";
+import { LoadingCircle } from "../components/etc/loadingCircle";
+import { activeSearchBtnStyle } from "../design/innerStyle";
+import { divisionNumbers } from "../components/etc/divisionLargeNumbers";
 // 전체
 
 const DEFAULT_CATEGORY = 0;
+const url = `http://elice-kdt-3rd-team-16.koreacentral.cloudapp.azure.com/`;
 
 export function SearchPage() {
   const [selectTags, setSelectTags] = useState([]);
@@ -90,9 +20,8 @@ export function SearchPage() {
   const [pageArray, setPageArray] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [videos, setVideos] = useState(null);
-  const url = `http://elice-kdt-3rd-team-16.koreacentral.cloudapp.azure.com/`;
-
   const [searchTags, setSearchTags] = useState([]);
+
   const handleCategory = categoryId => {
     setSelectCategory(categoryId);
   };
@@ -101,22 +30,18 @@ export function SearchPage() {
     const response = await axios.get(url + params);
     setSearchTags(response.data);
   };
-
   const handleTags = tagName => {
     const tagIndex = selectTags.indexOf(tagName);
     tagIndex === -1
-      ? setSelectTags([...selectTags, tagName])
+      ? setSelectTags([tagName, ...selectTags])
       : setSelectTags(selectTags.filter(item => item !== tagName));
   };
-
   const handlePageByCategory = async () => {
     const params = `/search/category?data= {'categoryId':${selectCategory},'page':${currentPage}}`;
     const response = await axios.get(url + params);
     setVideos(response.data.videos);
-
     setMaxPage(response.data.max_page);
   };
-
   //태그가 선택되면 호출
   const handlePageByTags = async () => {
     const params = `http://elice-kdt-3rd-team-16.koreacentral.cloudapp.azure.com/search/tags?data={'tags':'${selectTags.join(
@@ -142,7 +67,6 @@ export function SearchPage() {
       maxPage < 10
         ? Array.from({ length: maxPage }, (v, i) => i + 1)
         : Array.from({ length: 10 }, (v, i) => i + 1);
-
     setPageArray(temp);
   };
 
@@ -187,13 +111,14 @@ export function SearchPage() {
                 className="category-box-button"
                 id={`category-box-button${x.category_id}`}
                 onClick={e => {
+                  e.preventDefault();
                   handleCategory(x.category_id);
                   fetchTags(x.category_id);
                 }}
                 style={
                   `category-box-button${selectCategory}` ===
                   `category-box-button${x.category_id}`
-                    ? activeStyle
+                    ? activeSearchBtnStyle
                     : {}
                 }
               >
@@ -204,16 +129,17 @@ export function SearchPage() {
         </div>
         <div className="hashtag-box">
           {searchTags.length === 0 ? (
-            <div class="spinner-border text-danger" role="status">
-              <span class="sr-only">Loading...</span>
-            </div>
+            <LoadingCircle />
           ) : (
             searchTags.map(x => {
               return (
                 <button
                   id="hashtag-btn"
-                  style={selectTags.indexOf(x) !== -1 ? activeStyle : {}}
-                  onClick={() => {
+                  style={
+                    selectTags.indexOf(x) !== -1 ? activeSearchBtnStyle : {}
+                  }
+                  onClick={e => {
+                    e.preventDefault();
                     handleTags(x);
                   }}
                 >
@@ -224,7 +150,8 @@ export function SearchPage() {
           )}
           <span id="hastag-refresh-btn">
             <GrRefresh
-              onClick={() => {
+              onClick={e => {
+                e.preventDefault();
                 setSelectTags([]);
               }}
             />
@@ -234,9 +161,7 @@ export function SearchPage() {
 
       <div className="video-div">
         {videos === null ? (
-          <div className="spinner-border text-danger" role="status">
-            <span class="sr-only">Loading...</span>
-          </div>
+          <LoadingCircle />
         ) : (
           videos.map(v => {
             return (
@@ -275,20 +200,12 @@ export function SearchPage() {
                     <div className="video-likes-div">
                       <p id="video-views-p">
                         <GrLike />
-                        {`${
-                          v.likes > 1000000
-                            ? (v.likes / 1000000).toFixed(1)
-                            : (v.likes / 1000).toFixed(1)
-                        }${v.likes > 1000000 ? "M" : "K"}`}
+                        {divisionNumbers(v.likes, 1)}
                       </p>
                     </div>
                     <div className="video-views-div">
                       <GrView />
-                      <p id="video-likes-p">{`${
-                        v.views > 1000000
-                          ? (v.views / 1000000).toFixed(1)
-                          : (v.views / 1000).toFixed(1)
-                      }${v.views > 1000000 ? "M" : "K"}`}</p>
+                      <p id="video-likes-p">{divisionNumbers(v.views, 1)}</p>
                     </div>
                   </div>
                 </div>
@@ -320,7 +237,7 @@ export function SearchPage() {
                 </span>
               </a>
             </li>
-            {pageArray.map((x, index) => {
+            {pageArray.map(x => {
               return (
                 <li
                   class={`${
